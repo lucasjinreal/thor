43255443275b06a6c0472dd7e10decd5600b96a5
Your Name <you@example.com>
2019-07-05 17:32:54 +0800

    add

--- include/dl.h
+++ include/dl.h
@@ -19,18 +19,39 @@
 
 using namespace std;
 
+namespace thor {
+namespace dl {
 
-namespace thor{
+// add several CLASSES here
+vector<string> VOC_CLASSES = {"__background__",
+							  "aeroplane", "bicycle", "bird", "boat",
+							  "bottle", "bus", "car", "cat", "chair",
+							  "cow", "diningtable", "dog", "horse",
+							  "motorbike", "person", "pottedplant",
+							  "sheep", "sofa", "train", "tvmonitor"};
+vector<string> COCO_CLASSES = {
+	"__background__", "person", "bicycle", "car", "motorcycle", "airplane",
+	"bus", "train", "truck", "boat", "traffic light", "fire hydrant",
+	"stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
+	"sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack",
+	"umbrella", "handbag", "tie", "suitcase", "frisbee", "skis",
+	"snowboard", "sports ball", "kite", "baseball bat", "baseball glove",
+	"skateboard", "surfboard", "tennis racket", "bottle", "wine glass",
+	"cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich",
+	"orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
+	"chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv",
+	"laptop", "mouse", "remote", "keyboard", "cell phone", "microwave",
+	"oven", "toaster", "sink", "refrigerator", "book", "clock", "vase",
+	"scissors", "teddy bear", "hair drier", "toothbrush"
+};
 
-    namespace dl{
-
-        class BaseDetector{
-        public:
-            BaseDetector();
-            void preProcess(cv::Mat &img);
-            void detect();
-        };
-    }
+class BaseDetector {
+ public:
+  BaseDetector();
+  void preProcess(cv::Mat &img);
+  void detect();
+};
+}
 }
 
 #endif //CAO_CV_H
