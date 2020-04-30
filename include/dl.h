a4d76cb640b2672cdff78c74f3c387446543dd3d
lucasjin <aa@qq.com>
2020-04-30 21:32:26 +0800

    add voc classes

--- include/dl.h
+++ include/dl.h
@@ -52,6 +52,13 @@ vector<string> VOC_CLASSES = {"__background__",
 							  "cow", "diningtable", "dog", "horse",
 							  "motorbike", "person", "pottedplant",
 							  "sheep", "sofa", "train", "tvmonitor"};
+
+vector<string> VOC_CLASSES_NO_BK = {"aeroplane", "bicycle", "bird", "boat",
+							  "bottle", "bus", "car", "cat", "chair",
+							  "cow", "diningtable", "dog", "horse",
+							  "motorbike", "person", "pottedplant",
+							  "sheep", "sofa", "train", "tvmonitor"};
+
 vector<string> COCO_CLASSES = {
 	"__background__", "person", "bicycle", "car", "motorcycle", "airplane",
 	"bus", "train", "truck", "boat", "traffic light", "fire hydrant",
@@ -68,6 +75,20 @@ vector<string> COCO_CLASSES = {
 	"scissors", "teddy bear", "hair drier", "toothbrush"
 };
 
+vector<string> COCO_CLASSES_NO_BK = {"person", "bicycle", "car", "motorcycle", "airplane",
+	"bus", "train", "truck", "boat", "traffic light", "fire hydrant",
+	"stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
+	"sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack",
+	"umbrella", "handbag", "tie", "suitcase", "frisbee", "skis",
+	"snowboard", "sports ball", "kite", "baseball bat", "baseball glove",
+	"skateboard", "surfboard", "tennis racket", "bottle", "wine glass",
+	"cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich",
+	"orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
+	"chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv",
+	"laptop", "mouse", "remote", "keyboard", "cell phone", "microwave",
+	"oven", "toaster", "sink", "refrigerator", "book", "clock", "vase",
+	"scissors", "teddy bear", "hair drier", "toothbrush"
+};
 
 
 }
