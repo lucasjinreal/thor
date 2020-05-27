2adc65508193e5835e7060a708de2c88b01bd641
FagangJin <fagangjin@autox.ai>
2020-05-27 10:44:48 +0800

    add glob in thor::os

--- include/os.h
+++ include/os.h
@@ -28,62 +28,66 @@
 #ifndef _T_OS_H
 #define _T_OS_H
 
+#include <glob.h>    // glob(), globfree()
+#include <string.h>  // memset()
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <cassert>
 #include <iostream>
+#include <sstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <cassert>
-
 using namespace std;
 
-
-namespace thor{
-    namespace os{
+namespace thor {
+namespace os {
 //    using namespace ;
 
-    bool exists(string path);
-        bool isdir(string path);
-        bool isfile(string path);
+bool exists(string path);
+bool isdir(string path);
+bool isfile(string path);
 
-        vector<string> list_files(string path, bool full_path);
-        vector<string> list_files_recurse(string path, bool full_path);
-        vector<string> list_dirs(string path, bool full_path);
-        vector<string> list_all(string path, bool full_path);
+vector<string> list_files(string path, bool full_path);
+vector<string> list_files_recurse(string path, bool full_path);
+vector<string> list_dirs(string path, bool full_path);
+vector<string> list_all(string path, bool full_path);
 
-        string join(string path, string filename);
+string join(string path, string filename);
 
-        string parent_path(string path);
-        string filename(string path);
+string parent_path(string path);
+string filename(string path);
 
-        int do_mkdir(string path, mode_t mode);
-        int makedirs(string path, mode_t mode);
+int do_mkdir(string path, mode_t mode);
+int makedirs(string path, mode_t mode);
 
-        string abs_path(const string &path);
+string abs_path(const string &path);
 
-        string suffix(string path);
+string suffix(string path);
 
-        ///////////////// add some new APIs /////////////////////////
-        std::string GetAbsolutePath(const std::string &prefix,
-                                    const std::string &relative_path);
-        inline std::string GetFileName(const std::string &path) {
-          std::string filename;
-          std::string::size_type loc = path.rfind('/');
-          if (loc == std::string::npos) {
-            filename = path;
-          } else {
-            filename = path.substr(loc + 1);
-          }
-          return filename;
-        }
-
-
-
-    }
+///////////////// add some new APIs /////////////////////////
+std::string GetAbsolutePath(const std::string &prefix,
+                            const std::string &relative_path);
+inline std::string GetFileName(const std::string &path) {
+  std::string filename;
+  std::string::size_type loc = path.rfind('/');
+  if (loc == std::string::npos) {
+    filename = path;
+  } else {
+    filename = path.substr(loc + 1);
+  }
+  return filename;
 }
+
+// glob to find files by pattern
+
+std::vector<std::string> glob(const std::string &pattern);
+}  // namespace os
+}  // namespace thor
 
 // Define some micros to check the method params is null or not.
 #define CHECK_NULL(param) (if (param == NULL) cout << "got null.\n";)
 
-#endif //_OS_H
+#endif  //_OS_H
