76577e08210263431d3ad56d52f61316a109e0d9
FagangJin <fagangjin@autox.ai>
2020-12-31 12:55:11 +0800

    add new structure

--- include/structures.h
+++ include/structures.h
@@ -24,8 +24,9 @@
 #ifndef COMMON_STRUCTURE_H
 #define COMMON_STRUCTURE_H
 
-#include "iostream"
 #include <algorithm>
+
+#include "iostream"
 
 //#ifdef USE_OPENCV
 // #ifndef USE_OPENCV4
@@ -35,8 +36,6 @@
 //#endif
 
 using namespace std;
-
-
 
 /**
  *
@@ -64,9 +63,25 @@ enum BoxFormat {
   BOTH,
 };
 
+struct Bbox {
+  float xmin;
+  float ymin;
+  float xmax;
+  float ymax;
+  float score;
+  int cid;
+};
+struct InstanceSegmentation {
+  Bbox box;
+  // mask size uncertain
+  float *mask;
+  // vector<float> mask_vec;
+  // id for ith position in mem data of masks
+  int idx;
+};
+
 // simple box declaration
 struct Box {
-
   // left, top, right, bottom
   // left top is the origin
   int top;
@@ -86,45 +101,45 @@ struct Box {
 
   BoxFormat format;
 
-  Box(int a, int b, int c, int d, int format=BoxFormat::XYXY) {
-	switch (format) {
-	  case XYXY:
-	    xmin = a;
-		ymin = b;
-		xmax = c;
-		ymax = d;
-		this->format = XYXY;
-		break;
-	  case TLWH:
-	    top = a;
-		left = b;
-		w = c;
-		h = d;
-		this->format = TLWH;
-		break;
-	  default:
-	    break;
-	}
+  Box(int a, int b, int c, int d, int format = BoxFormat::XYXY) {
+    switch (format) {
+      case XYXY:
+        xmin = a;
+        ymin = b;
+        xmax = c;
+        ymax = d;
+        this->format = XYXY;
+        break;
+      case TLWH:
+        top = a;
+        left = b;
+        w = c;
+        h = d;
+        this->format = TLWH;
+        break;
+      default:
+        break;
+    }
   }
 
   void to_tlwh() {
-	// suppose we have xyxy already
-	if (format == XYXY) {
-	  this->top = xmin;
-	  this->left = ymin;
-	  this->w = xmax - xmin;
-	  this->h = ymax - ymin;
-	  format = BOTH;
-	}
+    // suppose we have xyxy already
+    if (format == XYXY) {
+      this->top = xmin;
+      this->left = ymin;
+      this->w = xmax - xmin;
+      this->h = ymax - ymin;
+      format = BOTH;
+    }
   }
   void to_xyxy() {
-	if (format == TLWH) {
-	  this->xmin = left;
-	  this->ymin = top;
-	  this->xmax = left + w;
-	  this->ymax = top + h;
-	  format = BOTH;
-	}
+    if (format == TLWH) {
+      this->xmin = left;
+      this->ymin = top;
+      this->xmax = left + w;
+      this->ymax = top + h;
+      format = BOTH;
+    }
   }
   void print() {
     switch (this->format) {
@@ -144,29 +159,27 @@ struct Box {
   }
 };
 
-struct BoxSimple{
+struct BoxSimple {
   float x1;
   float y1;
   float x2;
   float y2;
 };
 
-struct landmarks{
+struct landmarks {
   float x;
   float y;
 };
 
 // detection supports multiple usages
-struct Detection{
-  //x1 y1 x2 y2
+struct Detection {
+  // x1 y1 x2 y2
   BoxSimple bbox;
-  //float objectness;
+  // float objectness;
   landmarks marks[5];
   int classId;
   float prob;
 };
-
-
 
 //#ifdef USE_OPENCV
 // human pose decalaration
@@ -182,58 +195,52 @@ struct HumanPose {
   // if confirm it is face, this flag will on
   bool is_face = false;
 
-  HumanPose(const std::vector<cv::Point2f> &keypoints = std::vector<cv::Point2f>(),
-			const float &score = 0) : keypoints(keypoints),
-									  score(score) {};
+  HumanPose(
+      const std::vector<cv::Point2f> &keypoints = std::vector<cv::Point2f>(),
+      const float &score = 0)
+      : keypoints(keypoints), score(score){};
   Box to_box() {
-	const cv::Point2f absentKeypoint(-1.0f, -1.0f);
-	// converts human pose to a bounding box
-	vector<float> xss;
-	vector<float> yss;
-	for (auto const &p: keypoints) {
-	  // don't count absent points
-	  if (p == absentKeypoint) {
-		continue;
-	  }
-	  xss.push_back(p.x);
-	  yss.push_back(p.y);
-//	  cout << "(" << p.x << "," << p.y << ")" << "\n";
-	}
+    const cv::Point2f absentKeypoint(-1.0f, -1.0f);
+    // converts human pose to a bounding box
+    vector<float> xss;
+    vector<float> yss;
+    for (auto const &p : keypoints) {
+      // don't count absent points
+      if (p == absentKeypoint) {
+        continue;
+      }
+      xss.push_back(p.x);
+      yss.push_back(p.y);
+      //	  cout << "(" << p.x << "," << p.y << ")" << "\n";
+    }
 
-	auto xminmax = std::minmax_element(xss.begin(), xss.end());
-	auto yminmax = std::minmax_element(yss.begin(), yss.end());
-	Box b((int)*(xminmax.first),
-		  (int)*(yminmax.first),
-		  (int)*(xminmax.second),
-		  (int)*(yminmax.second), BoxFormat::XYXY);
-//	b.print();
-	return b;
+    auto xminmax = std::minmax_element(xss.begin(), xss.end());
+    auto yminmax = std::minmax_element(yss.begin(), yss.end());
+    Box b((int)*(xminmax.first), (int)*(yminmax.first), (int)*(xminmax.second),
+          (int)*(yminmax.second), BoxFormat::XYXY);
+    //	b.print();
+    return b;
   }
 };
 
 // todo: HumanFaces
 
-
 // camera intrinsics decalaration
 struct CameraIntrinsicParams {
   double fx, fy, cx, cy, scale;
   inline void print() {
-	std::cout << "fx: " << fx << " fy: " << fy << " cx: " << cx
-			  << " cy:" << cy << " scale: " << scale << endl;
+    std::cout << "fx: " << fx << " fy: " << fy << " cx: " << cx << " cy:" << cy
+              << " scale: " << scale << endl;
   };
   inline cv::Mat toCameraMatrix() {
-	// convert to a cv::Mat matrix
-	double camera_matrix_data[3][4] = {
-		{fx, 0, cx},
-		{0, fy, cy},
-		{0, 0, 1}
-	};
-	cv::Mat camera_mat(3, 3, CV_64F, camera_matrix_data);
-	return camera_mat;
+    // convert to a cv::Mat matrix
+    double camera_matrix_data[3][4] = {{fx, 0, cx}, {0, fy, cy}, {0, 0, 1}};
+    cv::Mat camera_mat(3, 3, CV_64F, camera_matrix_data);
+    return camera_mat;
   }
 };
 //#endif
 
-}
+}  // namespace thor
 
-#endif // COMMON_STRUCTURE_H
+#endif  // COMMON_STRUCTURE_H
